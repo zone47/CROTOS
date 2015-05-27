3f95a0b69ffb6104938d37f93d517e3ab50f3ffa
zone47 <benoit.deshayes@gmail.com>
2015-05-28 00:25:30 +0200

    Fix image link
    Fix image link on small screens

--- index.php
+++ index.php
@@ -71,6 +71,7 @@ function init_display(){
 				$(this).css("width",$(this).attr("data-width"));
 		});
 		$( ".thumb a" ).each(function( index ) {
+			$(this).prop("onclick", null);
 			this.href=$(this).attr("data-commons");
 			if (!($(this).hasClass("linksolo")))
 				$(this).children("img").attr("src",$(this).children("img").attr("data-img"));
@@ -79,6 +80,7 @@ function init_display(){
 	else{
 		$(".item").css("width","100%");
 		$( ".thumb a" ).each(function( index ) {
+			$(this).prop("onclick", null);
 			this.href=$(this).attr("data-file");
 		});
 	}
@@ -95,27 +97,6 @@ else{
 		echo "$(\"#iconot1\").attr(\"src\",\"img/arrow_up_day.png\");";
 }
 ?>	
-	/*$(".lien_notice").click(function() {
-		var id_not = "#notice"+this.id.replace("iconot","");
-		if($(id_not).css('display') == 'none'){
-			$(id_not).css({ display: "block" });
-<?php
-if ($disp==0)
-	echo "			$(\"#\"+this.id).attr(\"src\",\"img/arrow_up.png\");";
-else
-	echo "			$(\"#\"+this.id).attr(\"src\",\"img/arrow_up_day.png\");";
-?>
-		}
-		else{
-			$(id_not).css({ display: "none" });
-<?php
-if ($disp==0)
-	echo "			$(\"#\"+this.id).attr(\"src\",\"img/arrow_down.png\");";
-else
-	echo "			$(\"#\"+this.id).attr(\"src\",\"img/arrow_down_day.png\");";
-?>
-		}
-	});*/
 
 	var trueValues = [-40000,-30000,-20000,-10000,-8000,-6000,-4000,-3500,-3000,-2500,-2000,-1800,-1600,-1400,-1200,-1000,-800,-700,-600,-500,-400,-300,-200,-100,1,100,200,300,400,500,600,700,800,900,1000,1050,1100,1150,1200,1250,1300,1350,1400,1420,1440,1460,1480,1490,1500,1510,1520,1530,1540,1550,1560,1570,1580,1590,1600,1610,1620,1630,1640,1650,1660,1670,1680,1690,1700,1710,1720,1730,1740,1750,1760,1770,1780,1790,1800,1810,1820,1830,1840,1850,1860,1870,1880,1890,1900,1910,1920,1930,1940,1950,1960,1970,1980,1990,2000,2010,2015];
 	var values =[ 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100];
