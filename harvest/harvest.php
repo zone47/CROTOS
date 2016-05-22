dbcdcc41764bb48de17289dd8645872206bec1a5
zone47 <benoit.deshayes@gmail.com>
2016-05-22 20:02:29 +0200

    WDQ to SparQL
    New request for the scope (only item with image or Wikipedia article)
    and use of SparQL instead of WDQ

--- harvest/harvest.php
+++ harvest/harvest.php
@@ -1,114 +1,56 @@
 <?php
 /* / */
 /* Harvest WD */
-
 echo "\nHarvest";
+
 include $file_timer_begin;
 //$cmd="rm -Rf ".$fold_crotos."harvest/items";
 $cmd="del /Q ".str_replace("/","\\",$fold_crotos)."harvest\\items\\*.*";
 exec($cmd);
-//$cmd="mkdir ".$fold_crotos."harvest/items";
-//exec($cmd);
-/* items with subclasses
-3305213	 painting
-11060274 print
-133067   mosaic
-277583   kakemono
-12043905 pastel
-132137   icon
-5647631  handscroll
-170593   collage
-429785   poster
-46686    altarpiece
-5078274  sketch
-17584242 creative drawing
-42195 petroglypg
-=>4502142 art image
 
-860861   sculpture
-1278452  polyptych
-20437094   installation art
-//48498    illuminated manuscript
-125191   photograph
-326478   land art
-184296   tapestry
-572916   found object
-1473346  stained glass
-860372   digital art
-17514    graffiti
+$sparql="select DISTINCT ?item
+where {
+	{?item wdt:P31/wdt:P279* wd:Q4502142}        # art image
+	UNION {?item wdt:P31/wdt:P279* wd:Q860861}   # sculpture
+	UNION {?item wdt:P31/wdt:P279* wd:Q1278452}  # polyptych
+	UNION {?item wdt:P31/wdt:P279* wd:Q20437094} # installation art
+	UNION {?item wdt:P31/wdt:P279* wd:Q326478}   # land art
+	UNION {?item wdt:P31/wdt:P279* wd:Q184296}   # tapestry
+	UNION {?item wdt:P31/wdt:P279* wd:Q572916}   # found object
+	UNION {?item wdt:P31/wdt:P279* wd:Q1473346}  # stained glass
+	UNION {?item wdt:P31/wdt:P279* wd:Q860372}   # digital art
+	UNION {?item wdt:P31/wdt:P279* wd:Q17514}    # graffiti
+	UNION {?item wdt:P31/wdt:P279* wd:Q738680}   # pottery of ancient Greece
+	UNION {?item wdt:P31/wdt:P279* wd:Q15727816} # painting series
+	UNION {?item wdt:P31/wdt:P279* wd:Q18573970} # group of paintings
+	UNION {?item wdt:P31/wdt:P279* wd:Q19479037} # sculpture series
+	UNION {?item wdt:P31/wdt:P279* wd:Q19960510} # group of paintings
+	UNION {?item wdt:P31/wdt:P279* wd:Q16905563} # cycle of paintings
+	UNION {?item wdt:P31/wdt:P279* wd:Q125191.}  # photograph
+	UNION {?item wdt:P31 wd:Q220659}             # artifact
 
-
-738680  pottery of ancient Greece
-
-
-15727816 painting series
-18573970 group of paintings
-19479037 sculpture series
-19960510 series of prints
-16905563 cycle of paintings
-
-//1640824  inscription
-//178743   stele, subclass of artefact
-//178659 illustration
-
-items without subclasses
-//93184    drawing
-220659   artifact
-//35140   performance
-*/
-// Types with subclasses
-$types = array (4502142,860861,1278452,20437094,125191,326478,184296,572916,1473346,860372,17514,738680,15727816,18573970,19479037,19960510,16905563);
-
-// institutions with too much items without image
-$museum_min = "195:2983474,195:5476145,195:705551,195:671384,195:1464509,195:1192305,195:188740,195:1416890,195:49133,195:239303,195:160236,195:510324,195:430682,195:844926,195:2296362,195:526170,195:214867,195:1952033,195:842858,195:2817221,195:18572999,195:18573057,195:1471477,195:2365880,195:1948674,195:132783";
-
-//Photographs of National Library of Wales excluded
-$museum_excl = "%20and%20noclaim[195:666063]";
+	{?item wdt:P18 ?img.}                        # With image
+	UNION {                                      # OR
+	?article schema:about ?item .         
+	FILTER regex(str(?article), \"wikipedia\")}  # With Wikipedia article
+	UNION {                                      # OR
+	?item wdt:P195/wdt:P361* wd:Q19675}          # in the collections of the Louvre
+}";
+$sparqlurl=urlencode($sparql);
+$req="https://query.wikidata.org/sparql?format=json&query=".$sparqlurl;
+$res  = file_get_contents($req);
+$responseArray = json_decode($res,true);
 
 $cpt=0;
-for ($i=0;$i<count($types);$i++){
-	$type=$types[$i];
-	
-	for ($j=0;$j<2;$j++){
-		$req="http://wdq.wmflabs.org/api?q=claim[31:%28tree[".$type."][][279]%29]";
-		if ($j==0)
-			$req.="%20and%20noclaim[".$museum_min."]";
-		else
-			$req.="%20and%20claim[".$museum_min."]%20and%20claim[18]";
-		if ($type==125191)
-			$req.=$museum_excl;
-		$res = request($req);
-		$responseArray = json_decode($res,true);
-		foreach ($responseArray["items"] as $key => $value){
-			copy("https://www.wikidata.org/w/api.php?action=wbgetentities&ids=q$value&format=json", $fold_crotos."harvest/items/$value.json");
-			$cpt++;
-			if (($cpt % 1000)==0)
-				echo "\n$cpt";
-		}
-	}
+foreach ($responseArray["results"]["bindings"] as $key => $value){
+	$Qitem=$value["item"]["value"];
+	$numQ=str_replace("http://www.wikidata.org/entity/Q","",$Qitem);
+	copy("https://www.wikidata.org/w/api.php?action=wbgetentities&ids=q$numQ&format=json", $fold_crotos."harvest/items/$numQ.json");
+ 	$cpt++;
+	if (($cpt % 1000)==0)
+		echo "\n$cpt";
 }
-// Types without subclasses
-//$types2 = array (93184,220659,42195);
-$types2 = array (220659);
-for ($i=0;$i<count($types2);$i++){
-	$type=$types2[$i];
-	
-	for ($j=0;$j<2;$j++){
-		$req="http://wdq.wmflabs.org/api?q=claim[31:".$type."]";
-		if ($j==0)
-			$req.="%20and%20noclaim[".$museum_min."]";
-		else
-			$req.="%20and%20claim[".$museum_min."]%20and%20claim[18]";
-		$res = request($req);
-		$responseArray = json_decode($res,true);
-		foreach ($responseArray["items"] as $key => $value){
-			copy("https://www.wikidata.org/w/api.php?action=wbgetentities&ids=q$value&format=json", $fold_crotos."harvest/items/$value.json");
-			$cpt++;
-			if (($cpt % 500)==0)
-				echo "\n$cpt";
-		}
-	}
-}
+
 echo "\nHarvest done";
 include $file_timer_end;
 ?>
